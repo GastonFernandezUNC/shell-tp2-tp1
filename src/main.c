5940d09738294e0ecb4edd39c37c223de56f7590
gaston <gaston.fernandez@mi.unc.edu.ar>
2024-11-18 22:48:12 -0300

    Implemented 'cd -' Added 'clr' as an alias for 'clear' Added background execution

--- src/main.c
+++ src/main.c
@@ -1,99 +1,172 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
+// #include "handlers.h"
 
 #define MAX_CMD_LEN 1024
 #define MAX_CWD_BUFFER 1024
 
 #define MAX_ARGS 100
 
+char CWD[MAX_CWD_BUFFER], PWD[MAX_CWD_BUFFER], OLDPWD[MAX_CWD_BUFFER];
+char *USER, *HOSTNAME;
 
-char cwd[MAX_CWD_BUFFER];
+void getCurrentPath()
+{
 
-void curr_path(){
-    if(getcwd(cwd,sizeof(cwd)) == NULL){
+    if (getcwd(CWD, sizeof(CWD)) == NULL)
+    {
         perror("PATH LENGTH OUT OF BOUND");
     }
 
-    printf("%s$ ", cwd);
+    printf("%s@%s:%s$  ", USER, HOSTNAME, CWD);
 }
 
 // Function to read and parse the command
-void read_command(char *cmd) {
-    curr_path();
+void read_command(char* cmd)
+{
+    getCurrentPath();
     fgets(cmd, MAX_CMD_LEN, stdin);
-    cmd[strcspn(cmd, "\n")] = 0;  // Remove the newline at the end of the input
+    cmd[strcspn(cmd, "\n")] = 0; // Remove the newline at the end of the input
 }
 
 // Function to parse the command into program and arguments
-int parse_command(char *cmd, char **args) {
+int parse_command(char* cmd, char** args)
+{
     int i = 0;
-    args[i] = strtok(cmd, " ");  // Get the first argument
+    args[i] = strtok(cmd, " "); // Get the first argument
 
-    while (args[i] != NULL) {
+    while (args[i] != NULL)
+    {
         i++;
-        args[i] = strtok(NULL, " ");  // Get the next arguments
+        args[i] = strtok(NULL, " "); // Get the next arguments
     }
-    return i;
+    return i; // Return the number of arguments
 }
 
-void handle_cd(char **args) {
+void handle_cd(char** args)
+{
 
-    if (args[1] == NULL) {
+    if (args[1] == NULL || strcmp(args[1], "~") == 0)
+    {
         // If no argument is passed, change to the home directory
-        char *home = getenv("HOME");
-        if (home == NULL) {
+        char* home = getenv("HOME");
+        if (home == NULL)
+        {
             fprintf(stderr, "cd: HOME not set\n");
-
-        } else if (chdir(home) != 0) {
+        }
+        else if (chdir(home) != 0)
+        {
             perror("cd");
         }
+        strcpy(OLDPWD, home);
+    }
 
-    } else {
+    else
+    {
         // If a directory is passed, try to change to it
-        if (chdir(args[1]) != 0) {
-            perror("cd");
+        if (strcmp(args[1], "-") == 0)
+        {
+            // If the argument is "-", change to the previous directory
+
+            // Swap the current directory with the old directory
+            char tmp[MAX_CWD_BUFFER];
+            strcpy(tmp, PWD);
+            strcpy(PWD, OLDPWD);
+            strcpy(OLDPWD, tmp);
+
+            if (chdir(PWD) != 0)
+            {
+                perror("cd");
+            }
+        }
+        else
+        {
+            // Save the current directory as the old directory
+            getcwd(OLDPWD, sizeof(OLDPWD));
+            // Change to the specified directory
+            if (chdir(args[1]) != 0)
+            {
+                perror("cd");
+            }
+            // Save the new directory as the current directory
+            getcwd(PWD, sizeof(OLDPWD));
         }
     }
 }
 
 // Main function
-int main() {
-    char cmd[MAX_CMD_LEN];  // Command buffer
-    char *args[MAX_ARGS];   // Array of arguments
+int main()
+{
+    char cmd[MAX_CMD_LEN]; // Command buffer
+    char* args[MAX_ARGS];  // Array of arguments
+    USER = getenv("USER");
+    HOSTNAME = getenv("HOSTNAME");
 
-    while (1) {
-        read_command(cmd);  // Get the command from user
-        if (strlen(cmd) == 0) continue;  // Skip empty input
+    while (1)
+    {
+        read_command(cmd); // Get the command from user
+        if (strlen(cmd) == 0)
+            continue; // Skip empty input
 
         // Parse the command into program and arguments
         int arg_count = parse_command(cmd, args);
 
         // Check for exit condition
-        if (strcmp(args[0], "exit") == 0 ){
+        if (strcmp(args[0], "exit") == 0)
+        {
             break;
         }
 
         // Check for change directory
-        if (strcmp(args[0], "cd") == 0) {
+        if (strcmp(args[0], "cd") == 0)
+        {
             handle_cd(args);
-            continue;  // Skip forking and executing a program
+            continue; // Skip forking and executing a program
+        }
+
+        if (strcmp(args[0], "clr") == 0)
+        {
+            strcpy(args[0], "clear");
+        }
+
+        bool background = false;
+        if (strcmp(args[arg_count - 1], "&") == 0)
+        {
+            args[arg_count - 1] = NULL;
+            background = true;
         }
 
         // Create a child process to execute the command
         pid_t pid = fork();
-        if (pid == 0) {
-            // In tyhe child process: Execute the command
-            if (execvp(args[0], args) == -1) {
+        if (pid == 0)
+        {
+            // In the child process: Execute the command
+            if (execvp(args[0], args) == -1)
+            {
                 perror("execvp failed");
                 exit(1);
             }
-        } else if (pid > 0) {
+        }
+        else if (pid > 0)
+        {
             // In the parent process: Wait for the child to finish
-            wait(NULL);
-        } else {
+            if (!background)
+            {
+                // wait(NULL);
+                waitpid(pid, NULL, 0);
+            }
+            else
+            {
+                printf("Process running in background with PID %d\n", pid);
+                background = false;
+            }
+        }
+        else
+        {
             perror("fork failed");
             exit(1);
         }
